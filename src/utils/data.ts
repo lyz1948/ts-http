d532368e724b19b4ac088e710c418f3f2caace82
lyz <lvqinbo@163.com>
2019-05-10 23:31:07 +0800

    format response data

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -3,3 +3,14 @@ import { isPlainObject } from './index'
 export function transformRequest(data: any): any {
   return isPlainObject(data) ? JSON.stringify(data) : data
 }
+
+export function transformResponse(data: any): any {
+  if (typeof data === 'string') {
+    try {
+      data = JSON.parse(data)
+    } catch (error) {
+      // do nothing
+    }
+  }
+  return data
+}
